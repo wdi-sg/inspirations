392db26204b674e6d9251e565d903ef94f769294
Teo Boon Hock <hockie2@yahho.com.sg>
2019-07-29 23:22:38 +0800

    Did til without the user login

---
+++ seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO quotes(quote) VALUES('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+INSERT INTO quotes(quote) VALUES('Aenean varius sapien eu tortor sagittis tempor ut ac ipsum.');
+INSERT INTO quotes(quote) VALUES('Sed laoreet massa at feugiat consectetur.');
