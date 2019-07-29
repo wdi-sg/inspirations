15e3540fadfd82483690c1da30d89066e17aef12
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-07-29 23:05:57 +0800

    Created main page that shows all quotes. Quote can be inserted if using form method.

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("we are in the browser");
+
+var button = document.getElementById("submit");
+
+button.addEventListener('click', function(event){
+    var input = document.querySelector('#inputquote').value;
+    console.log("input" + input);
+    newquote(input);
+});
+
+let newquote = function(input){
+    console.log("in newquote func" + input);
+
+    // make a new request
+    var request = new XMLHttpRequest();
+
+    // listen for the request response
+    request.addEventListener("load", responseHandler);
+
+
+};
+
+// what to do when we receive the request
+var responseHandler = function() {
+
+    let output = document.getElementById('output');
+    output.innerHTML += "<li>"+JSON.parse(this.responseText).id+"</li>";
+
+    console.log("response text", this.responseText);
+    console.log("status text", this.statusText);
+    console.log("status code", this.status);
+};
+
+// ready the system by calling open, and specifying the url
+var url = "/";
+request.open("POST", url);
+request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+// send the request
+request.send();
---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO quotes(quote) VALUES('Dream bigger');
+INSERT INTO quotes(quote) VALUES('“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.” – Winston Churchill');
+INSERT INTO quotes(quote) VALUES('“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.” – Vince Lombardi');
+INSERT INTO quotes(quote) VALUES('“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.” – Rob Siltanen');
+INSERT INTO quotes(quote) VALUES('“Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do.” – Johann Wolfgang Von Goethe');
