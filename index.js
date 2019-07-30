a8d68196ee4b4534cae9982180ac1d0d71507a9d
axejacob <email@axeljacob.com>
2019-07-30 09:14:20 +0800

    V1

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const reactEngine = require('express-react-views').createEngine();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactEngine);
+
+const db = require('./db');
+const routeCallBack = require('./routes');
+
+routeCallBack(app, db);
+
+const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+let onClose = function(){
+
+  console.log("closing");
+
+  server.close(() => {
+
+    console.log('Process terminated');
+
+    pool.end( () => console.log('Shut down db connection pool'));
+  })
+};
+
+process.on('SIGTERM', onClose);
+process.on('SIGINT', onClose);
